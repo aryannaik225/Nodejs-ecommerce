29334f2f6a9ba05ec6ea65b3eade8ad73781acb8
aryannaik225 <aryann2203@gmail.com>
2026-01-09 13:49:53 +0530

    converted auth mysql code to prisma orm

--- crud/db/user-queries.js
+++ crud/db/user-queries.js
@@ -1,10 +1,15 @@
 import { pool } from "./index.js";
 
 export const findUserByEmail = async (email) => {
-  const QUERY = "SELECT * FROM users WHERE email = ?";
+  // const QUERY = "SELECT * FROM users WHERE email = ?";
   try {
-    const [rows] = await pool.query(QUERY, [email]);
-    return rows[0];
+    // const [rows] = await pool.query(QUERY, [email]);
+    // return rows[0];
+
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+    });
+    return user;
   } catch (error) {
     console.log("Error finding user", error);
     throw error;
@@ -12,10 +17,19 @@ export const findUserByEmail = async (email) => {
 };
 
 export const createUser = async (name, email, password) => {
-  const QUERY = "INSERT INTO users (name, email, password) VALUES (? , ?, ?)"
+  // const QUERY = "INSERT INTO users (name, email, password) VALUES (? , ?, ?)"
   try {
-    const [result] = await pool.query(QUERY, [name, email, password])
-    return result;
+    // const [result] = await pool.query(QUERY, [name, email, password])
+    // return result;
+
+    const newUser = await prisma.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password,
+      },
+    });
+    return newUser;
   } catch (error) {
     console.log("Error creating user", error)
     throw error;
